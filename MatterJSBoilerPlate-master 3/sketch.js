005e9b2484258709cc9fb714b484a97ce00ae44d
DarkPanda6448 <70301715+DarkPanda6448@users.noreply.github.com>
2020-11-23 19:42:29 -0800

    Update sketch.js

--- MatterJSBoilerPlate-master 3/sketch.js
+++ MatterJSBoilerPlate-master 3/sketch.js
@@ -41,18 +41,60 @@ function setup() {
 	  width: 1200,
 	  height: 700,
 	 wireframes: false
-	
+		
+		
+	rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
+	rope2=new rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
+	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
+	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
+	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
+
+
 	}
-}
-
-
+		
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(230);
+  roofObject.display();
+
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()	
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+ 
   
-  drawSprites();
+  
+	
+  
+ 
+  
+  
  
 }
 
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+
+  	}
+}
 
 
+function drawLine(constraint)
+{
+	bobBodyPosition=constraint.bodyA.position
+	roofBodyPosition=constraint.bodyB.position
+
+	roofBodyOffset=constraint.pointB;
+	
+	roofBodyX=roofBodyPosition.x+roofBodyOffset.x
+	roofBodyY=roofBodyPosition.y+roofBodyOffset.y
+	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
+}
