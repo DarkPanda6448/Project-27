21380a6c986e60fe3d743b489f2f2c5eb40e7866
DarkPanda6448 <70301715+DarkPanda6448@users.noreply.github.com>
2020-11-23 17:52:17 -0800

    Update sketch.js

--- MatterJSBoilerPlate-master 3/sketch.js
+++ MatterJSBoilerPlate-master 3/sketch.js
@@ -3,11 +3,12 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint
 
-function preload()
-{
-	
-}
+var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5, roofObject
+var rope1, rope2, rope3, rope4, rope5;
+var world; 
 
 function setup() {
 	createCanvas(800, 700);
@@ -16,11 +17,23 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
-
 
 	Engine.run(engine);
-  
+	
+	roofObject=new roof(width/2,height/4,width/7,20);
+
+	bobDiameter=40;
+	
+	startBobPositionX=width/2;
+	startBobPositionY=height/4+500;
+	
+	bobObject1=new bob(startBobPositionX-bobDiameter*2, startBobPositionY, bobDiameter);
+	bobObject2=new bob(startBobPositionX-bobDiameter, startBobPositionY, bobDiameter);
+	bobObject3=new bob(startBobPositionX, startBobPositionY, bobDiameter);
+	bobObject4=new bob(startBobPositionX+bobDiameter, startBobPositionY, bobDiameter);
+	bobObject5=new bob(startBobPositionX+bobDiameter*2, startBobPositionY, bobDiameter);
+	
+	var render = Render.create
 }
 
 
